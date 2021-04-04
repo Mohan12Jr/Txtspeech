81ee02681a7497e0f8f511530b96e66d472e5416
mohan12Jr <80115806+Mohan12Jr@users.noreply.github.com>
2021-04-04 11:16:39 +0530

    Add files via upload

---
+++ TextToSpeech.py
@@ -0,0 +1,4 @@
+import pyttsx3
+speaker=pyttsx3.init()
+speaker.say("Hello,This is mohan I am very excited to create new projects based on Computer science Folllow me for my updates ")
+speaker.runAndWait()
